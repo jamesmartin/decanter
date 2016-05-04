603274ad4fd458a3704513e29bbdbe6ded0dd7a6
James Martin <jamesmartin@github.com>
2016-05-04 18:12:39 +1000

    Add clj-http v2 as a dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/jamesmartin/decanter"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.1.0"]]
   :main ^:skip-aot decanter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
