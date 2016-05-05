8c265233ccceef715f59bffd88528ed6083993e3
James Martin <jamesmartin@github.com>
2016-05-05 14:42:53 +1000

    Get username from argv

--- src/decanter/core.clj
+++ src/decanter/core.clj
@@ -3,12 +3,21 @@
 
 (require '[clojure.tools.cli :refer [cli]])
 
-(def arg-defs ["-h" "--help" "Print this help" :default false :flag true])
+(def arg-defs [["-h" "--help" "Print this help" :default false :flag true]
+               ["-u" "--username" :default false :flag false]])
+
+(def required-opts #{:username})
+(defn missing-required?
+  "Returns true if opts is missing any of its required-opts"
+  [opts]
+  (not-every? opts required-opts))
 
 (defn -main [& args]
-  (let [[opts args banner] (cli args arg-defs)]
-    (when (:help opts)
-      (println banner)))
+  (let [[opts args banner] (apply cli args arg-defs)]
+    (when (or (:help opts)
+              (missing-required? opts))
+      (println banner))
+    (println (str "Username: " (:username opts))))
 
   "I don't do a whole lot ... yet."
-  (println "Hello, World!"))
+  (println "Hello, Wine!"))
