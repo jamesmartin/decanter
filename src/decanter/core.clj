accb83cc4daacdef9cfcf9a45ad2d42df6ec0ecd
James Martin <jamesmartin@github.com>
2016-05-05 14:44:26 +1000

    Passwords are required

--- src/decanter/core.clj
+++ src/decanter/core.clj
@@ -4,9 +4,10 @@
 (require '[clojure.tools.cli :refer [cli]])
 
 (def arg-defs [["-h" "--help" "Print this help" :default false :flag true]
-               ["-u" "--username" :default false :flag false]])
+               ["-u" "--username" :default false :flag false]
+               ["-p" "--password" :default false :flag false]])
 
-(def required-opts #{:username})
+(def required-opts #{:username :password})
 (defn missing-required?
   "Returns true if opts is missing any of its required-opts"
   [opts]
