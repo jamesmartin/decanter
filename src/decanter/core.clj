cc787c2632a8c5440a3a2395ee3e917401f1be7f
James Martin <jamesmartin@github.com>
2016-05-05 14:25:44 +1000

    Clearer when arg definitions are separate

--- src/decanter/core.clj
+++ src/decanter/core.clj
@@ -3,11 +3,12 @@
 
 (require '[clojure.tools.cli :refer [cli]])
 
+(def arg-defs ["-h" "--help" "Print this help" :default false :flag true])
+
 (defn -main [& args]
-  (let [[opts args banner] (cli args
-                                ["-h" "--help" "Print this help"
-                                 :default false :flag true])]
+  (let [[opts args banner] (cli args arg-defs)]
     (when (:help opts)
       (println banner)))
+
   "I don't do a whole lot ... yet."
   (println "Hello, World!"))
