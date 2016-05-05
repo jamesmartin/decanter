2be2110c4adb8f737c8066e9dad61770ad259e6c
James Martin <jamesmartin@github.com>
2016-05-05 14:14:36 +1000

    tools for parsing command line args

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.1.0"]]
+                 [clj-http "2.1.0"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot decanter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/decanter/core.clj
+++ src/decanter/core.clj
@@ -1,7 +1,13 @@
 (ns decanter.core
   (:gen-class))
 
-(defn -main
+(require '[clojure.tools.cli :refer [cli]])
+
+(defn -main [& args]
+  (let [[opts args banner] (cli args
+                                ["-h" "--help" "Print this help"
+                                 :default false :flag true])]
+    (when (:help opts)
+      (println banner)))
   "I don't do a whole lot ... yet."
-  [& args]
   (println "Hello, World!"))
