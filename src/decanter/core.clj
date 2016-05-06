b33c13a7984436abf6d6c29ce2df0f95a53d793c
James Martin <jamesmartin@github.com>
2016-05-06 17:52:25 +1000

    Test the homepage on startup

--- src/decanter/core.clj
+++ src/decanter/core.clj
@@ -3,6 +3,8 @@
 
 (require '[clojure.tools.cli :refer [cli]])
 
+(defn wine-xchange-url [] "https://www.wine-xchange.com.au")
+
 (def arg-defs [["-h" "--help" "Print this help" :default false :flag true]
                ["-u" "--username" :default false :flag false]
                ["-p" "--password" :default false :flag false]])
@@ -13,6 +15,16 @@
   [opts]
   (not-every? opts required-opts))
 
+(require '[clj-http.client :as http])
+
+(defn homepage-test [& fake-conn?]
+  (if fake-conn? "Fake it 'til you make it..."
+    (try
+      (let [response (http/get (wine-xchange-url))]
+         (str "Homepage OK: " (:status response)))
+      (catch Exception e (str "homepage unavailable: " (.getMessage e))))
+    ))
+
 (defn -main [& args]
   (let [[opts args banner] (apply cli args arg-defs)]
     (when (or (:help opts)
@@ -21,4 +33,4 @@
     (println (str "Username: " (:username opts))))
 
   "I don't do a whole lot ... yet."
-  (println "Hello, Wine!"))
+  (println (homepage-test)))
